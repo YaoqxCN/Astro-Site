e0c533bafdd384d71e493be619ea19eff44df1ea
Yaoqx <1064245198@qq.com>
2024-08-29 22:55:16 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -12,7 +12,7 @@ export const USER_NAME = "Yaoqx";
 export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
-export const SERVER_URL = "https://demo.saroprock.com";
+export const SERVER_URL = "https://yaoqx-comment.netlify.app/.netlify/functions/comment";
 
 
 // Some informative text on the site
